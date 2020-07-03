8502a19279a8c352b0b06abb8397606eeda430e8
MayaMua <owojw95@gmail.com>
2020-07-02 22:35:20 -0500

    add recursion solution to 94

--- LeetCode/Java/94.binary-tree-inorder-traversal.java
+++ LeetCode/Java/94.binary-tree-inorder-traversal.java
@@ -35,36 +35,53 @@ import javax.swing.tree.TreeNode;
 
 class Solution {
     // 迭代法
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ret = new ArrayList<>();
-        if(root == null) {
-            return ret;
-        }
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> ret = new ArrayList<>();
+    //     if(root == null) {
+    //         return ret;
+    //     }
 
     
-        // 栈
-        Stack<TreeNode> stack = new Stack<>();        
-        // 定义指针遍历
-        TreeNode cur = root;
-        // 指针不能和栈同时为空
-        while(!stack.isEmpty() || cur!=null) {
-            // 找到树的最左边的叶子
-            while(cur != null) {
-                stack.push(cur);
-                cur = cur.left;
-            }
+    //     // 栈
+    //     Stack<TreeNode> stack = new Stack<>();        
+    //     // 定义指针遍历
+    //     TreeNode cur = root;
+    //     // 指针不能和栈同时为空
+    //     while(!stack.isEmpty() || cur!=null) {
+    //         // 找到树的最左边的叶子
+    //         while(cur != null) {
+    //             stack.push(cur);
+    //             cur = cur.left;
+    //         }
 
-            // 如果此时指针指向的是最左边的叶子，则先输出
-            // 如果不是，则说明找到了之前经历过的中间的root的位置，同样将其输出
-            // 并进一步把指针指向root的右子树
-            TreeNode node = stack.pop();
-            ret.add(node.val);
-            cur = node.right;
+    //         // 如果此时指针指向的是最左边的叶子，则先输出
+    //         // 如果不是，则说明找到了之前经历过的中间的root的位置，同样将其输出
+    //         // 并进一步把指针指向root的右子树
+    //         TreeNode node = stack.pop();
+    //         ret.add(node.val);
+    //         cur = node.right;
 
 
+    //     }
+
+    //     return ret;
+    // }
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
         }
 
-        return ret;
+    public void inorder(TreeNode root, List<Integer>res) {
+        if(root==null)
+            return;
+
+        
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
     }
 }
 // @lc code=end

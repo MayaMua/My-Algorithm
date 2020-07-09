657ac3a665dcbcf1cc536ccf761e2724fb8f79c2
MayaMua <owojw95@gmail.com>
2020-07-09 07:28:37 -0500

    3sum Brute Force

--- LeetCode/Java/15.3-sum.java
+++ LeetCode/Java/15.3-sum.java
@@ -7,7 +7,23 @@
 // @lc code=start
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        if(nums == null || nums.length <= 2) {
+            return Collections.emptyList();
+        }
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new LinkedHashSet<>();
+        for (int i = 0; i < nums.length-2; i++) {
+            for (int j = i+1; j < nums.length-1; j++) {
+                for (int k = j+1; k<nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> value = Arrays.asList(nums[i], nums[j], nums[k]);  // 排序放好
+                        result.add(value);
+                     }
+                }
+            }
+        }
         
+        return new ArrayList<>(result);
     }
 }
 // @lc code=end

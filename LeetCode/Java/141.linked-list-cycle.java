caf472cada1c3c21c43542216b3f922215a8fe9a
MayaMua <owojw95@gmail.com>
2020-07-09 22:09:10 -0500

    cycle in linked list

---
+++ LeetCode/Java/141.linked-list-cycle.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;    
+        }
+
+        return true;
+    }
+
+
+    // public boolean hasCycle(ListNode head) {
+    //     if (head == null || head.next == null) {
+    //         return false;
+    //     }
+    //     ListNode slow = head;
+    //     ListNode fast = head;
+    //     while (slow != fast) {
+    //         return false;
+    //     }
+    //     slow = slow.next;
+    //     fast = fast.next.next;
+    // }
+
+    
+}
+// @lc code=end
+

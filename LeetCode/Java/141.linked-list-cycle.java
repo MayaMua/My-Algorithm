16898c8db3024c6d417897cd97dd1418091f4744
MayaMua <owojw95@gmail.com>
2020-07-09 22:13:40 -0500

    #141 hash and double pointers

--- LeetCode/Java/141.linked-list-cycle.java
+++ LeetCode/Java/141.linked-list-cycle.java
@@ -17,36 +17,36 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) {
-            return false;
-        }
-        ListNode slow = head;
-        ListNode fast = head.next;
-        while (slow != fast) {
-            if (fast == null || fast.next == null) {
-                return false;
-            }
-            slow = slow.next;
-            fast = fast.next.next;    
-        }
-
-        return true;
-    }
-
-
     // public boolean hasCycle(ListNode head) {
     //     if (head == null || head.next == null) {
     //         return false;
     //     }
     //     ListNode slow = head;
-    //     ListNode fast = head;
+    //     ListNode fast = head.next;
     //     while (slow != fast) {
-    //         return false;
+    //         if (fast == null || fast.next == null) {
+    //             return false;
+    //         }
+    //         slow = slow.next;
+    //         fast = fast.next.next;    
     //     }
-    //     slow = slow.next;
-    //     fast = fast.next.next;
+
+    //     return true;
     // }
+
+
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new LinkedHashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+
+    }
 
     
 }

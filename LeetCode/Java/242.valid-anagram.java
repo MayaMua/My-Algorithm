86b669ff3d8bfb93dc03807ca25086877ea22fbc
MayaMua <owojw95@gmail.com>
2020-07-17 17:12:19 -0500

    0717 work

--- LeetCode/Java/242.valid-anagram.java
+++ LeetCode/Java/242.valid-anagram.java
@@ -24,10 +24,38 @@ class Solution {
         // }
         // return true;
 
+//===========================================================
+        // if (s.length() != t.length()) {
+        //     return false;
+        // }
 
-
+        // int[] count_s = new int [26];
+        // int[] count_t = new int [26];
         
+        // for (int i = 0; i < s.length(); i++) {
+        //     count_s[s.charAt(i) - 'a']++;
+        //     count_t[t.charAt(i) - 'a']++;
+        // }
+        // for (int i = 0; i < 26; i++) {						//比较两字符表是否相同
+        //     if (count_s[i] != count_t[i]) return false;
+        // }
 
+        // return true;
+//==================================================================
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
 
     }
 }
